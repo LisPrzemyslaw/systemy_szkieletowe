4eb929c7d317eaef4a07039dd766dac3a03d7d75
LisPrzemyslaw <przemek_lis@onet.eu>
2024-06-10 22:06:38 +0200

    add ahref

--- src/main/resources/META-INF/resources/endpointScript.js
+++ src/main/resources/META-INF/resources/endpointScript.js
@@ -1,7 +1,8 @@
 function goToNextPage() {
     const username = document.getElementById('username').value;
     if (username) {
-        alert("Good username: " + username);
+        document.cookie = `username=${encodeURIComponent(username)};path=/;`;
+        window.location.href = 'http://localhost:8080/reservation';
     } else {
         alert('Please enter your username.');
     }
